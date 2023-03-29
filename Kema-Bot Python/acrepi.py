f09ab770c0ddfe224f48cb4cc6afafd78c9a7597
LoonBac21 <68069341+loonbac@users.noreply.github.com>
2023-03-28 19:12:50 -0500

    Python Add
    se agregaron los archivos de python para poder funcionar

---
+++ Kema-Bot Python/acrepi.py
@@ -0,0 +1,39 @@
+# Importar la librería de discord.py
+import os
+import discord
+from discord.ext import commands
+
+# Importar la clase Intents
+from discord import Intents
+
+# Crear una instancia de Intents con todos los intents activados
+intents = Intents.all()
+
+# Crear una instancia del bot con el prefijo "!" y los intents
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# Definir el ID del rol que quieres contar
+role_id = 1087851758342123630
+
+@bot.event
+async def on_ready():
+    print("Buscando usuarios con Acrepi")
+    
+# Definir un comando que envíe el número de usuarios con el rol
+@bot.command(name="acrepi")
+async def count(ctx):
+  # Obtener el rol por su ID
+  role = ctx.guild.get_role(role_id)
+  # Si el rol existe
+  if role:
+    # Contar cuántos miembros tienen el rol
+    count = len(role.members)
+    # Enviar un mensaje con el resultado
+    await ctx.send(f"Hay {count} Usuarios usando Acrepi :D")
+  # Si el rol no existe
+  else:
+    # Enviar un mensaje de error
+    await ctx.send(f"No se encontró el rol con el ID {role_id}.")
+
+# Ejecutar el bot con tu token
+bot.run("MTA5MDA3NDExODA3NTk5MDAyNg.GIMZfS.jQmIIWiKg9z1m0LN1m1SVdAWdcn79E50cWdqTY")
---
+++ Kema-Bot Python/fun.py
@@ -0,0 +1,51 @@
+import discord
+import re
+import os
+from discord.ext import commands
+
+# Importar la clase Intents
+from discord import Intents
+
+# Crear una instancia de Intents con todos los intents activados
+intents = Intents.all()
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+client = commands.Bot(command_prefix="!", intents=intents)  # Cambia el prefijo como desees
+
+xd_count = 0  # Contador de mensajes que contienen "xd"
+
+@client.event
+async def on_ready():
+    print("Preparando comandos Troll 7u7")
+
+@client.event
+async def on_message(message):
+    global xd_count
+
+    # Verificar si el mensaje contiene "xd" en cualquiera de sus formas
+    if "xd" in message.content.lower() and message.author != client.user:
+        xd_count += 1
+        await message.channel.send(f"XD Numero {xd_count} uwu")
+      # Verificar si el mensaje contiene "multicuenta" o "multicuentas"
+    if "multicuenta" in message.content.lower() or "multicuentas" in message.content.lower():
+        # Obtener la ruta absoluta del archivo de la imagen
+        image_path = os.path.abspath("imagen.jpg")
+        # Cargar la imagen desde la ruta del archivo
+        file = discord.File(image_path, filename="imagen.jpg")
+        # Enviar la imagen como archivo adjunto en un mensaje
+        await message.channel.send(file=file)
+
+      # Verificar si el mensaje solo contiene la palabra "que"
+    if re.match(r'^que$', message.content.lower()) and message.author != client.user:
+        await message.channel.send("so, te trolie jeje nya")
+      # Verificar si el mensaje solo contiene la palabra "a"
+    if re.match(r'^a$', message.content.lower()) and message.author != client.user:
+        await message.channel.send("rroz .¿")
+        # Cariño comandos
+    if client.user.mentioned_in(message) and any(word in message.content.lower() for word in ["te amo", "te quiero", "cariño"]):
+        await message.channel.send("Y-yo tambien te quiero mucho uwu.")
+
+    await client.process_commands(message)
+  
+client.run("MTA5MDA3NDExODA3NTk5MDAyNg.GIMZfS.jQmIIWiKg9z1m0LN1m1SVdAWdcn79E50cWdqTY")
